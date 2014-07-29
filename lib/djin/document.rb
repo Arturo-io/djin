23b831d775b96991204c9f0c979b07257faa2fdc
Sumeet Singh <ortuna@gmail.com>
2014-07-29 09:53:40 -0400

    Missing manifests

--- lib/djin/document.rb
+++ lib/djin/document.rb
@@ -1,9 +1,29 @@
 module Djin
+  class MissingManifest < StandardError; end
   class Document
-    attr_reader :repository
+    attr_reader   :repository
+    attr_accessor :base
 
     def initialize(params = {})
       @repository = Repository.new(params)
+    end
+
+    def execute
+      check_missing_manifest
+    end
+
+    def clone
+      repository.clone
+    end
+
+    private
+    def local_path
+      repository.repo.path
+    end
+
+    def check_missing_manifest
+      return true if File.exists?("#{local_path}/#{base}/arturo.yml")
+      raise Djin::MissingManifest, "arturo.yml could not be found in the project"
     end
   end
 end
--- spec/djin/document_spec.rb
+++ spec/djin/document_spec.rb
@@ -9,6 +9,28 @@ describe Djin::Document do
     it 'can create an instance correctly' do
       instance = subject.new(remote: remote, token: token)
       expect(instance.repository).not_to be_nil
+      expect(instance.base).to eq(nil)
     end
+
+    it 'bubbles exceptions from repository' do
+      expect { subject.new(token: token) }.to raise_error
+    end
+
   end
+
+  context 'single clone' do
+    before do
+      @instance = subject.new(remote: remote, token: token)
+      @instance.clone
+    end
+
+
+    describe '#execute' do
+      it 'verifies a manifest exists' do
+        @instance.base = "missing_manifest"
+        expect { @instance.execute }.to raise_error(Djin::MissingManifest)
+      end
+    end
+
+  end 
 end
